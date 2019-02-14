bb2013dd1e654a8f58a4ae11f00b0306d649e8ad
Zhongrui Xu <zhongruixu@gmail.com>
2019-02-14 14:48:17 +0800

    ListView With Trailed Heart

---
+++ listview_practice/lib/listview_heart.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class ListViewHeart extends StatefulWidget {
+  @override
+  ListViewHeartState createState() => new ListViewHeartState();
+}
+
+class ListViewHeartState extends State<ListViewHeart> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('widget'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (context, i) {
+      // 如果 i 为奇数 返回 divider
+      if (i.isOdd)
+        return Divider(
+          color: Colors.red,
+        );
+
+      // 如果 i 为偶数 返回 _buildRow的结果
+      //TODO index 表达式的含义??
+      final index = i ~/ 2;
+
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title:new Text(
+        pair.asPascalCase,
+        style: TextStyle(fontSize: 10),
+      ),
+
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ?Colors.red : null,
+      ),
+
+      // onTap 的实现方式
+      onTap: () {
+        /// 在 Flutter 的响应式风格框架中，调用 setState() ，将为 State 对象触发 build() 方法的调用，从而实现对UI的更新。
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+
+    );
+  }
+}
--- listview_practice/lib/main.dart
+++ listview_practice/lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+//import 'package:firebase_analytics/firebase_analytics.dart';
+import 'listview_heart.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+//  FirebaseAnalytics _analytics = FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new MaterialApp(
+
       title: 'Welcom to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new AppBar(
-            title: new Text('Welcome to Flutter'),
-          ),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
+      home: new ListViewHeart(),
+      theme: new ThemeData(
+        primaryColor: Colors.red,
       ),
     );
   }
@@ -29,11 +28,64 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+
+  /// Prefixing an identifier with an underscore enforces privacy in the Dart language.
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (context,i) {
+//      print(context);
+//      print('i = $i');
+      if (i.isOdd) return Divider(height: 20, color: Colors.blue);
+//      if (i.isEven) return Divider(height: 44,color: Colors.red);
+      final index = i ~/ 2;
+//      print(index);
+      // 如果 index  大于当前 suggestions 的数量 那么向 suggestions 添加数据
+      if (index >= _suggestions.length) {
+        //
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile (
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        /// Notify the framework that the internal state of this object has changed.The provided callback is immediately called synchronously
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          }else {
+            _saved.add(pair);
+          }
+
+        });
+      },
+    );
+
   }
 
 }
